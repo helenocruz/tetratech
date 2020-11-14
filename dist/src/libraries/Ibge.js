046bbfe30c963ce89795d63da0bf095f057ca6f1
Heleno Cruz <helenocruz@hotmail.com.br>
2020-11-14 10:22:06 -0300

    Acrescimo de verificações para validação do parâmetro 'datetime'

--- dist/src/libraries/Ibge.js
+++ dist/src/libraries/Ibge.js
@@ -7,11 +7,16 @@ var Ibge = /** @class */ (function () {
         if (datetime_search === void 0) { datetime_search = '01012050120000'; }
         this.url_ibge_projecao_populacao = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao";
         this.dataSearch_Utc = 0;
+        this.current_date_Utc = 0;
+        this.datetime_search_Date = new Date();
+        var current_date = new Date();
+        current_date = new Date(current_date.getTime() - (current_date.getTimezoneOffset() * 60 * 1000));
+        this.current_date_Utc = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDay(), current_date.getHours(), current_date.getMinutes(), current_date.getSeconds());
         var date = datetime_search.substring(4, 8) + '-' + datetime_search.substring(2, 4) + '-' + datetime_search.substring(0, 2);
         var time = datetime_search.substring(8, 10) + ':' + datetime_search.substring(10, 12) + ':' + datetime_search.substring(12, 14);
-        var datetime_search_Date = new Date(date + ' ' + time);
-        datetime_search_Date = new Date(datetime_search_Date.getTime() - (datetime_search_Date.getTimezoneOffset() * 60 * 1000));
-        this.dataSearch_Utc = Date.UTC(datetime_search_Date.getFullYear(), datetime_search_Date.getMonth(), datetime_search_Date.getDay(), datetime_search_Date.getHours(), datetime_search_Date.getMinutes(), datetime_search_Date.getSeconds());
+        this.datetime_search_Date = new Date(date + ' ' + time);
+        this.datetime_search_Date = new Date(this.datetime_search_Date.getTime() - (this.datetime_search_Date.getTimezoneOffset() * 60 * 1000));
+        this.dataSearch_Utc = Date.UTC(this.datetime_search_Date.getFullYear(), this.datetime_search_Date.getMonth(), this.datetime_search_Date.getDay(), this.datetime_search_Date.getHours(), this.datetime_search_Date.getMinutes(), this.datetime_search_Date.getSeconds());
     }
     Ibge.prototype.calcProjecaoPopulacional = function (projecaoPopulacaoAtual, additionalTime_Utc, incrementoPopulacional) {
         if (projecaoPopulacaoAtual === void 0) { projecaoPopulacaoAtual = 0; }
@@ -39,6 +44,9 @@ var Ibge = /** @class */ (function () {
     Ibge.prototype.getProjecaoPopulacional = function () {
         var _this = this;
         return new Promise(function (resolve, reject) {
+            if (_this.current_date_Utc < 0 || isNaN(_this.datetime_search_Date.valueOf())) {
+                reject({ status: 400, data: { error: "O parâmetro de 'datetime' informado está incorreto." } });
+            }
             _this.getDataIbge()
                 .then(function (dataIbge) {
                 if (typeof dataIbge !== 'object') {
@@ -49,14 +57,13 @@ var Ibge = /** @class */ (function () {
                 var time = dateReturned.substring(11, 13) + ':' + dateReturned.substring(14, 16) + ':' + dateReturned.substring(17, 19);
                 var dateIBGE = new Date(date + ' ' + time);
                 dateIBGE = new Date(dateIBGE.getTime() - (dateIBGE.getTimezoneOffset() * 60 * 1000));
+                if (isNaN(dateIBGE.valueOf())) {
+                    reject({ status: 500, data: { error: 'Erro na data informada por API do IBGE.' } });
+                }
                 var dateIBGE_Utc = Date.UTC(dateIBGE.getFullYear(), dateIBGE.getMonth(), dateIBGE.getDay(), dateIBGE.getHours(), dateIBGE.getMinutes(), dateIBGE.getSeconds());
                 var diffBetweenDates_Utc = _this.dataSearch_Utc - dateIBGE_Utc;
-                console.log(diffBetweenDates_Utc);
-                if (diffBetweenDates_Utc < 0) {
-                    reject({ status: 400, data: { error: 'A data informada é anterior a data atual.' } });
-                }
                 var projecaoPopulacaoFutura = _this.calcProjecaoPopulacional(dataIbge.projecao.populacao, diffBetweenDates_Utc, dataIbge.projecao.periodoMedio.incrementoPopulacional); //fazer teste neste retorno
-                resolve({ status: 200, data: { projecao: projecaoPopulacaoFutura, data: dateIBGE } });
+                resolve({ status: 200, data: { projecao: projecaoPopulacaoFutura, data_projecao: _this.datetime_search_Date, data_pesquisa: dateIBGE } });
             })
                 .catch(function (error) {
                 reject({ status: 500, data: error });
--- src/libraries/Ibge.ts
+++ src/libraries/Ibge.ts
@@ -2,17 +2,23 @@ import { getData } from './GlobalRest';
 
 export class Ibge {
 
-    url_ibge_projecao_populacao : String = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao";
-    dataSearch_Utc : number = 0;
+    protected url_ibge_projecao_populacao : String = "https://servicodados.ibge.gov.br/api/v1/projecoes/populacao";
+    protected dataSearch_Utc : number = 0;
+    protected current_date_Utc: number = 0;
+    protected datetime_search_Date : Date = new Date();
 
     constructor(datetime_search : String = '01012050120000'){
+        let current_date: Date = new Date();
+        current_date = new Date(current_date.getTime() - (current_date.getTimezoneOffset()*60*1000));
+        this.current_date_Utc = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDay(), current_date.getHours(), current_date.getMinutes(), current_date.getSeconds());
+
         let date : String = datetime_search.substring(4,8) + '-' + datetime_search.substring(2,4) + '-' + datetime_search.substring(0,2);
         let time : String = datetime_search.substring(8,10) + ':' + datetime_search.substring(10,12) + ':' + datetime_search.substring(12,14);
         
-        let datetime_search_Date : Date = new Date(date+' '+time);
-        datetime_search_Date = new Date(datetime_search_Date.getTime() - (datetime_search_Date.getTimezoneOffset()*60*1000));
+        this.datetime_search_Date = new Date(date+' '+time);
+        this.datetime_search_Date = new Date(this.datetime_search_Date.getTime() - (this.datetime_search_Date.getTimezoneOffset()*60*1000));
         
-        this.dataSearch_Utc = Date.UTC(datetime_search_Date.getFullYear(), datetime_search_Date.getMonth(), datetime_search_Date.getDay(), datetime_search_Date.getHours(), datetime_search_Date.getMinutes(), datetime_search_Date.getSeconds());
+        this.dataSearch_Utc = Date.UTC(this.datetime_search_Date.getFullYear(), this.datetime_search_Date.getMonth(), this.datetime_search_Date.getDay(), this.datetime_search_Date.getHours(), this.datetime_search_Date.getMinutes(), this.datetime_search_Date.getSeconds());
     }
 
     private calcProjecaoPopulacional(projecaoPopulacaoAtual : number = 0, additionalTime_Utc : number = 0, incrementoPopulacional : number = 0) : number | boolean {
@@ -37,6 +43,9 @@ export class Ibge {
 
     getProjecaoPopulacional(): Promise<object> {
         return new Promise((resolve, reject)=>{
+            if(this.current_date_Utc < 0 || isNaN(this.datetime_search_Date.valueOf())){
+                reject({status: 400, data: {error: "O parâmetro de 'datetime' informado está incorreto."}});
+            }
             this.getDataIbge()
             .then((dataIbge : any)=>{
                 if(typeof dataIbge !== 'object') {
@@ -48,19 +57,17 @@ export class Ibge {
                 let date : String = dateReturned.substring(6,10) + '-' + dateReturned.substring(3,5) + '-' + dateReturned.substring(0,2); 
                 let time : String = dateReturned.substring(11,13) + ':' + dateReturned.substring(14,16) + ':' + dateReturned.substring(17,19);
                 
-                let dateIBGE = new Date(date+' '+time);
+                let dateIBGE = new Date(date+' '+time); 
                 dateIBGE = new Date(dateIBGE.getTime() - (dateIBGE.getTimezoneOffset()*60*1000));
-                let dateIBGE_Utc : number = Date.UTC(dateIBGE.getFullYear(), dateIBGE.getMonth(), dateIBGE.getDay(), dateIBGE.getHours(), dateIBGE.getMinutes(), dateIBGE.getSeconds());
-                
-                let diffBetweenDates_Utc = this.dataSearch_Utc - dateIBGE_Utc;
-                console.log(diffBetweenDates_Utc);
-                if(diffBetweenDates_Utc < 0){
-                    reject({status: 400, data: {error: 'A data informada é anterior a data atual.'}});
+                if(isNaN(dateIBGE.valueOf())){
+                    reject({status: 500, data: {error: 'Erro na data informada por API do IBGE.'}});
                 }
+                let dateIBGE_Utc : number = Date.UTC(dateIBGE.getFullYear(), dateIBGE.getMonth(), dateIBGE.getDay(), dateIBGE.getHours(), dateIBGE.getMinutes(), dateIBGE.getSeconds());
+                let diffBetweenDates_Utc = this.dataSearch_Utc - dateIBGE_Utc;
 
                 let projecaoPopulacaoFutura = this.calcProjecaoPopulacional(dataIbge.projecao.populacao, diffBetweenDates_Utc, dataIbge.projecao.periodoMedio.incrementoPopulacional); //fazer teste neste retorno
                 
-                resolve({status: 200, data: {projecao: projecaoPopulacaoFutura, data: dateIBGE}});
+                resolve({status: 200, data: {projecao: projecaoPopulacaoFutura, data_projecao: this.datetime_search_Date, data_pesquisa: dateIBGE}});
             })
             .catch((error)=>{
                 reject({status: 500, data: error});
